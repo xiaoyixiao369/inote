d41b9d7d0872bed172aedd02331fc5c096a0ff57
Gordon Shaw <igordonshaw@gmail.com>
2015-04-12 12:55:28 +0800

    change value for sql auto_increment

--- dbinit/inote.sql
+++ dbinit/inote.sql
@@ -19,7 +19,7 @@ CREATE TABLE `post` (
   `thumb` varchar(500) DEFAULT NULL,
   `tag` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `message`;
 CREATE TABLE `message` (
@@ -30,7 +30,7 @@ CREATE TABLE `message` (
   `post_id` int(11) NOT NULL,
   `post_title` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=42 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 INSERT INTO `user` (`id`, `user_name`, `password`, `thumb`, `about_me`, `site_title`, `head_bg_pic`) VALUES ('1', 'Gordon', 'c3284d0f94606de1fd2af172aba15bf3', '/static/uploads/D3784F4E-4879-4ED0-BBE5-7D9DD3D70B6F.png', 'iNote是一款开源，免费，简洁的单页博客程序', 'iNote', null);
 
